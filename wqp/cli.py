1202d9611c68b619bc018e4bdeaa9ed5d894333e
Eric Hu <hueric1997@gmail.com>
2024-08-29 23:38:09 +0200

    Add jobs train to cli

--- wqp/cli.py
+++ wqp/cli.py
@@ -1,4 +1,6 @@
 from wqp import __version__
+from wqp.workflow import model_training_workflow
+
 import click 
 
 @click.group()
@@ -12,5 +14,10 @@ def jobs():
     pass
 
 @jobs.command()
-def train():
-    pass
+@click.option(
+            '--data-path',
+            default='http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv',
+            help='train the model with the given path or a default path'
+            )
+def train(data_path:str):
+    model_training_workflow(data_path=data_path)

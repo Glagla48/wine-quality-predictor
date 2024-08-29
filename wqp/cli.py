6210c442116d28749503b4034356026c8bd1f470
Eric Hu <hueric1997@gmail.com>
2024-08-29 18:39:25 +0200

    Change wqp cli

--- wqp/cli.py
+++ wqp/cli.py
@@ -1,5 +1,16 @@
+from wqp import __version__
+import click 
+
 @click.group()
 @click.version_option(__version__)
 @click.pass_context
-def cli(ctx):
+def wqp(ctx):
+    pass
+
+@wqp.group()
+def jobs():
+    pass
+
+@jobs.command()
+def train():
     pass
